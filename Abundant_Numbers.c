35fede3b05f7039cf2470c1ef626da3c82ef6bbc
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-02-13 08:05:06 +0530

    #include<stdio.h> int main() {     int n,i,fs=0;     scanf("%d",&n);     for(i=1;i<n;i++)     {         if(n%i==0)         {             fs=fs+i;         }     }     if(fs>n)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Abundant_Numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,fs=0;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            fs=fs+i;
+        }
+    }
+    if(fs>n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");        
+    }
+}
