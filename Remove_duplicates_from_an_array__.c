28114661eaaf7c3087dc4e930fe3b8a720288e9e
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-03-06 19:08:08 +0530

    #include<stdio.h> int main() {     int n,a[100],i,j,k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         for(j=i+1;j<n;j++)         {             if(a[i]==a[j])             {                 for(k=j;k<n-1;k++)                 {                     a[k]=a[k+1];                 }                 n--;                 j--;             }         }     }     for(i=0;i<n;i++)     {         printf("%d ",a[i]);     }
    }

---
+++ Remove_duplicates_from_an_array__.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,j,k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                for(k=j;k<n-1;k++)
+                {
+                    a[k]=a[k+1];
+                }
+                n--;
+                j--;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    
+}
