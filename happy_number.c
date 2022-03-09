2f5c66c2a6bd0100814358e10c5634a5764a88f0
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-03-09 18:49:14 +0530

    #include<stdio.h> int main() {     int n,s=0,d;     scanf("%d",&n);     while(s!=1 && s!=4)     {         s=0;         while(n>0)         {             d=n%10;             s=s+(d*d);             n=n/10;         }         n=s;     }     if(s==1)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ happy_number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,s=0,d;
+    scanf("%d",&n);
+    while(s!=1 && s!=4)
+    {
+        s=0;
+        while(n>0)
+        {
+            d=n%10;
+            s=s+(d*d);
+            n=n/10;
+        }
+        n=s;
+    }
+    if(s==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
