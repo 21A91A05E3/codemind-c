3013147803963256f8e813a7de0f15b89f010f1e
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-03-22 19:06:51 +0530

    #include<stdio.h> int main(){    int num, rem, reverse_num, temp, start, end;    scanf("%d",&start);    scanf("%d",&end);    for(num=start;num<=end;num++){       temp=num;       reverse_num=0;       while(temp){          rem=temp%10;          temp=temp/10;          reverse_num=reverse_num*10+rem;       }       if(num==reverse_num)          printf("%d ",num);    }    return 0; }

---
+++ Program_to_print_all_palindromes_in_a_given_range_.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+   int num, rem, reverse_num, temp, start, end;
+   scanf("%d",&start);
+   scanf("%d",&end);
+   for(num=start;num<=end;num++){
+      temp=num;
+      reverse_num=0;
+      while(temp){
+         rem=temp%10;
+         temp=temp/10;
+         reverse_num=reverse_num*10+rem;
+      }
+      if(num==reverse_num)
+         printf("%d ",num);
+   }
+   return 0;
+}
