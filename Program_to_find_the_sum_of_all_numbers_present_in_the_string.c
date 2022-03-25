e57057bc9076c252adeb40f5ca53126c5e9129ba
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-03-25 20:25:49 +0530

    #include<stdio.h> int main() {     char a[100];     int s=0,i;     scanf("%s",a);     for(i=0;a[i]!=NULL;i++)     {         if(a[i]>=48 && a[i]<=57)         {             s=s+(a[i]-48);         }     }     printf("%d",s);
    }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char a[100];
+    int s=0,i;
+    scanf("%s",a);
+    for(i=0;a[i]!=NULL;i++)
+    {
+        if(a[i]>=48 && a[i]<=57)
+        {
+            s=s+(a[i]-48);
+        }
+    }
+    printf("%d",s);
+    
+}
