e27b66af1fa7f7706d87f763c69ee5b95d3cc90e
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-04-12 19:21:03 +0530

    #include<stdio.h> int main() {     int n,d,s=0,i;     scanf("%d",&n);     for(i=1;i<n;i++)     {         if(n%i==0)         {             s=s+i;         }     }     if(s==n)     {         printf("True");     }     else     {         printf("False");     } }

--- Perfect_Number.c
+++ Perfect_Number.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,s=0;
+    int n,d,s=0,i;
     scanf("%d",&n);
     for(i=1;i<n;i++)
     {
@@ -18,5 +18,4 @@ int main()
     {
         printf("False");
     }
-
 }
