665addc2d3cdb5a13eaff0106308d738897487de
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-04-12 19:38:52 +0530

    #include<stdio.h> int main() {     int n,d,temp,s=0,p=1;     scanf("%d",&n);     temp=n;     while(n)     {     	d=n%10;     	n=n/10;     	s=s+d; 	} 	n=temp; 	while(n)     {     	d=n%10;     	n=n/10;     	p=p*d; 	} 	if(p==s) 	{ 		printf("Spy Number"); 	} 	else 	{ 		printf("Not Spy Number"); 	} 	return 0; }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,22 +1,29 @@
 #include<stdio.h>
 int main()
 {
-    int n,d,s=0,m=1;
+    int n,d,temp,s=0,p=1;
     scanf("%d",&n);
+    temp=n;
     while(n)
     {
-        d=n%10;
-        n=n/10;
-        s=s+d;
-        m=m*d;
-    }
-    if(s==m)
+    	d=n%10;
+    	n=n/10;
+    	s=s+d;
+	}
+	n=temp;
+	while(n)
     {
-        printf("Spy Number");
-    }
-    else
-    {
-        printf("Not Spy Number");        
-    }
-    
+    	d=n%10;
+    	n=n/10;
+    	p=p*d;
+	}
+	if(p==s)
+	{
+		printf("Spy Number");
+	}
+	else
+	{
+		printf("Not Spy Number");
+	}
+	return 0;
 }
