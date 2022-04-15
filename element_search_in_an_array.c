85d623514276952c058b72495b924f22429497ca
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-04-15 22:33:40 +0530

    #include<stdio.h> int main() {     int n,a[1000],i,k,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         if(a[i]==k)         {             c++;             break;         }     }     if(c!=0)     {         printf("True");     }     else     {         printf("False");     }
    }

---
+++ element_search_in_an_array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[1000],i,k,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+            c++;
+            break;
+        }
+    }
+    if(c!=0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    
+}
