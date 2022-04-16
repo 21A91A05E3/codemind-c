3bf8822a9d0c4d914269899a75efbe33b8b69579
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-04-16 11:18:58 +0530

    #include<stdio.h> int main() { 	int i,n,a[100],c=0,dc=0,j,k; 	scanf("%d",&n); 	for(i=0;i<n;i++) 	{ 		scanf("%d",&a[i]); 	} 	scanf("%d",&k); 	for(i=0;i<n;i++) 	{ 		c=0; 		if(a[i]>=k) 		{ 			for(j=1;j<=a[i];j++) 			{ 				if(a[i]%j==0) 				{ 					c++; 				} 			} 			if(c==2) 			{ 				dc++; 			} 		} 	} 	printf("%d ",dc); }

---
+++ Primes_greater_than_k.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,a[100],c=0,dc=0,j,k;
+	scanf("%d",&n);      
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);   
+	}
+	scanf("%d",&k);
+	for(i=0;i<n;i++)    
+	{ 	
+		c=0;
+		if(a[i]>=k)
+		{
+			for(j=1;j<=a[i];j++)
+			{
+				if(a[i]%j==0)    
+				{
+					c++;
+				}
+			}
+			if(c==2)
+			{
+				dc++;
+			}	
+		}
+	}
+	printf("%d ",dc);
+}
