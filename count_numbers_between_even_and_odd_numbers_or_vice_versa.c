64ca9bb5db06780dcb9183b42c887a62bbb594fe
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-04-16 10:08:57 +0530

    #include<stdio.h> int main() {     int n,a[100],i,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=1;i<n-1;i++)     {         if((a[i-1]%2==0 && a[i+1]%2!=0)||(a[i-1]%2!=0 && a[i+1]%2==0))         {             c++;         }
        }
        printf("%d",c);
    }

---
+++ count_numbers_between_even_and_odd_numbers_or_vice_versa.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<n-1;i++)
+    {
+        if((a[i-1]%2==0 && a[i+1]%2!=0)||(a[i-1]%2!=0 && a[i+1]%2==0))
+        {
+            c++;
+        }
+        
+    }
+    printf("%d",c);
+}
