5e0153771e6c40b0178268d32eb3ff4e285adc63
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-04-16 11:13:08 +0530

    #include<stdio.h> int main() {     int n,a[100],i,s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             break;         }         else         {             s=s+a[i];         }     }     printf("%d",s); }

---
+++ sum_of_array__elements_upto__first_even_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,s=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            break;
+        }
+        else
+        {
+            s=s+a[i];
+        }
+    }
+    printf("%d",s);
+}
