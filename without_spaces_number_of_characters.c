6a445ee0dda4994736695256fd388219ce2877f2
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-04-17 10:26:57 +0530

    #include<stdio.h> int main() {     char str[100],i,c=0;     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]==' ')         {             continue;         }         else         {             c++;         }     }     printf("%d",c); }

---
+++ without_spaces_number_of_characters.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    char str[100],i,c=0;
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]==' ')
+        {
+            continue;
+        }
+        else
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
