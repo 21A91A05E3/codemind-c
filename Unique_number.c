3884013a7768ea912d31ea0453db382cd7642f69
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-04-19 21:14:17 +0530

    #include<stdio.h> int main() {     int n,d[100],dc,i=0,j,t,flag=0;     scanf("%d",&n);     t=n;     while(n)     {         d[i]=n%10;         n=n/10;         dc++;         i++;     }     for(i=0;i<dc;i++)     {         for(j=0;j<dc;j++)         {             if(i!=j && d[i]==d[j] )             {                 flag=1;                 break;             }         }     }     if(flag==1)     {         printf("Not Unique Number");     }     else     {         printf("Unique Number");     } }

---
+++ Unique_number.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,d[100],dc,i=0,j,t,flag=0;
+    scanf("%d",&n);
+    t=n;
+    while(n)
+    {
+        d[i]=n%10;
+        n=n/10;
+        dc++;
+        i++;
+    }
+    for(i=0;i<dc;i++)
+    {
+        for(j=0;j<dc;j++)
+        {
+            if(i!=j && d[i]==d[j] )
+            {
+                flag=1;
+                break;
+            }
+        }
+    }
+    if(flag==1)
+    {
+        printf("Not Unique Number");
+    }
+    else
+    {
+        printf("Unique Number");
+    }
+}
+
