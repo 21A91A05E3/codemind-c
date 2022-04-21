78c3b20bd4636eedfe231b0ea0326be2c73b1135
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-04-21 10:37:18 +0530

    #include<stdio.h> int main() {     int n,a[100],i,e[100],o[100],ec=0,oc=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             e[ec]=a[i];             ec++;         }         else         {             o[oc]=a[i];             oc++;         }     }     for(i=0;i<oc;i++)     {         printf("%d ",o[i]);     }     for(i=0;i<ec;i++)     {         printf("%d ",e[i]);     }
    }

---
+++ array_of_add_and_even_elements.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,e[100],o[100],ec=0,oc=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            e[ec]=a[i];
+            ec++;
+        }
+        else
+        {
+            o[oc]=a[i];
+            oc++;
+        }
+    }
+    for(i=0;i<oc;i++)
+    {
+        printf("%d ",o[i]);
+    }
+    for(i=0;i<ec;i++)
+    {
+        printf("%d ",e[i]);
+    }
+
+}
