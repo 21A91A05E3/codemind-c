74cb77d181d1a61bf1f42c05a3dd77a74461a8e0
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-04-21 19:01:58 +0530

    #include<stdio.h> int main() {     int n,ar[100],i,a,b,c=0,m=99999;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(ar[i]<a || ar[i]>b)         {             continue;         }         else         {             c++;             if(ar[i]<=m)             {                 m=ar[i];             }
            }
        }
        if(c==0)
        {
            printf("-1");
        }
        else
        {
            printf("%d",m);
        }
    }

---
+++ minimum_between_A_and_B.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n,ar[100],i,a,b,c=0,m=99999;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(ar[i]<a || ar[i]>b)
+        {
+            continue;
+        }
+        else
+        {
+            c++;
+            if(ar[i]<=m)
+            {
+                m=ar[i];
+            }
+            
+        }
+    }
+    if(c==0)
+    {
+        printf("-1");
+    }
+    else
+    {
+        printf("%d",m);
+    }
+}
