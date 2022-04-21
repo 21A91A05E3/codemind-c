09fa16d28cc6a00a0a8dacf624a93bd69bfc240e
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-04-21 18:58:31 +0530

    #include<stdio.h> int main() {     int n,ar[100],i,a,b,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(ar[i]<a || ar[i]>b)         {             continue;         }         else         {             c++;             printf("%d ",ar[i]);         }     }     if(c==0)     {         printf("-1");     } }

---
+++ elements_in_between_A_and_B.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,ar[100],i,a,b,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(ar[i]<a || ar[i]>b)
+        {
+            continue;
+        }
+        else
+        {
+            c++;
+            printf("%d ",ar[i]);
+        }
+    }
+    if(c==0)
+    {
+        printf("-1");
+    }
+}
