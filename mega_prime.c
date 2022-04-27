daa99ca80f9766fef9f39010e1d228d87afddc8b
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-04-27 17:43:22 +0530

    #include<stdio.h> int prime(int n) {     int c=0,i;     for(i=1;i<=n;i++)     {         if(n%i==0)         {             c++;         }     }     return c; }
    int main()
    {
        int n,d,p,dc=0,dp,s=0;
        scanf("%d",&n);
        p=prime(n);
        if(p==2)
        {
            while(n)
            {
                dc++;
                d=n%10;
                dp=prime(d);
                {
                    if(dp==2)
                    {
                        s++;
                    }
                }
                n=n/10;
            }
            if(s==dc)
            {
                printf("Mega Prime");
            }
            else
            {
                printf("Not Mega Prime");
            }
        }
        else
        {
            printf("Not Mega Prime");
        }
    
    }

---
+++ mega_prime.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int prime(int n)
+{
+    int c=0,i;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+        }
+    }
+    return c;
+}
+
+int main()
+{
+    int n,d,p,dc=0,dp,s=0;
+    scanf("%d",&n);
+    p=prime(n);
+    if(p==2)
+    {
+        while(n)
+        {
+            dc++;
+            d=n%10;
+            dp=prime(d);
+            {
+                if(dp==2)
+                {
+                    s++;
+                }
+            }
+            n=n/10;
+        }
+        if(s==dc)
+        {
+            printf("Mega Prime");
+        }
+        else
+        {
+            printf("Not Mega Prime");
+        }
+    }
+    else
+    {
+        printf("Not Mega Prime");
+    }
+
+}
