16a8968ca99ddfe5181530e86b05661834021417
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-05-02 18:29:50 +0530

    #include<stdio.h> int main() {     int n,a[1000],i,j,s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)         {             if(i!=j && a[i]==a[j])             {                 a[j]=-1;             }         }     }     for(j=0;j<n;j++)     {         if(a[j]!=-1 && a[j]%2==0)         {             s++;         }
        }
        printf("%d",s);
    }

---
+++ unique_even_count.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[1000],i,j,s=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i!=j && a[i]==a[j])
+            {
+                a[j]=-1;
+            }
+        }
+    }
+    for(j=0;j<n;j++)
+    {
+        if(a[j]!=-1 && a[j]%2==0)
+        {
+            s++;
+        }
+        
+    }
+    printf("%d",s);
+}
