d86af7baf5233a94526d52dae26fe34aaa091c96
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2022-08-24 10:01:27 +0530

    #include <stdio.h> int main() {
        int n,i,j,k,m; 
        scanf ("%d",&n);   
        for ( i = 1; i <= n; i++)  
        {  
            for ( j = 1; j <= n-i; j++)  
            {     
                printf (" ");   
            }  
            for ( k=i-1; k>=0; k--)  
            {  
                printf ("%d", k); 
            }  
            for (m =1; m<=i-1; m++)  
            {  
                printf("%d", m);
            }  
            printf ("
    ");  
        }  
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,29 +1,24 @@
-#include<stdio.h>
-int main()
-{
-    int n,i,j;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=n;j++)
-        {
-            if(i<n)
-            {
-                if((j==1 || j==i))
-                {
-                    printf("*");
-                }
-                else
-                {
-                    printf(" ");
-                }
-            }
-            if(i==n && j<=n)
-            {
-                printf("*");
-            }
-        }
-        printf("
-");
-    }
-}
+#include <stdio.h>  
+int main()  
+{  
+  
+    int n,i,j,k,m; 
+    scanf ("%d",&n);   
+    for ( i = 1; i <= n; i++)  
+    {  
+        for ( j = 1; j <= n-i; j++)  
+        {     
+            printf (" ");   
+        }  
+        for ( k=i-1; k>=0; k--)  
+        {  
+            printf ("%d", k); 
+        }  
+        for (m =1; m<=i-1; m++)  
+        {  
+            printf("%d", m);
+        }  
+        printf ("
+");  
+    }  
+}  
